27c18ec70cf2d8d549e9500cc7803685c10e4f59
chiranjeev kundu <chiranjeevkundu2000@gmail.com>
2022-11-26 23:49:52 +0530

    first item is added

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,24 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iphone 14 pro",
+        desc: "Apple iphone 16 Gen",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstore.storeimages.cdn-apple.com%2F4668%2Fas-images.apple.com%2Fis%2Fiphone-14-pro-model-unselect-gallery-1-202209%3Fwid%3D5120%26hei%3D2880%26fmt%3Dp-jpg%26qlt%3D80%26.v%3D1660753619946&imgrefurl=https%3A%2F%2Fwww.apple.com%2Fin%2Fshop%2Fbuy-iphone%2Fiphone-14-pro&tbnid=xkLVUZzvTo_AcM&vet=12ahUKEwi60rG0icz7AhX6oNgFHVBvCjEQMygAegUIARCgAg..i&docid=FmiZDA68gfUBLM&w=5120&h=2880&itg=1&q=iphone%2014%20pro%20max&client=safari&ved=2ahUKEwi60rG0icz7AhX6oNgFHVBvCjEQMygAegUIARCgAg"
+    ),
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,28 @@
+import 'package:day1/models/catalog.dart';
 import 'package:flutter/material.dart';
-
 import '../Widgets/drawer.dart';
+import '../Widgets/item_widget.dart';
 
-//day 11 learned about contraints and context
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "Chiranjeev";
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text(context.runtimeType.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length ,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
-        // Container(
-        // constraints: BoxConstraints(
-        //   minHeight: 70,
-        //   maxHeight: 200,
-        //   minWidth: 70,
-        //   maxWidth: 200,
-        // ),
-        // color: Colors.green,
-        // child: Container(
-        //   height: 10,
-        //   width: 20,
-        //   color: Colors.amber,
-        // ),
-        // ),
       drawer: MyDrawer(),
     );
   }
